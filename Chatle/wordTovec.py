cb79796e0697b4a363d3746c2d5f2e3ca3899e7f
CFaiter <42732153+CFaiter@users.noreply.github.com>
2023-03-13 20:32:50 +0900

    init

---
+++ Chatle/wordTovec.py
@@ -0,0 +1,24 @@
+import gensim
+import time
+
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+
+start = time.time()
+# モデルをロード
+model = gensim.models.KeyedVectors.load_word2vec_format('./content/model.vec',binary=False)
+end = time.time()
+# 解答
+ans = "ナポリタン"
+# qで終了
+while(True):
+    start1 = time.time()
+    w1 = input("Enter word 1(かな or カナ or 漢字): ")
+    if w1 == "q":
+        break
+    print(f'{w1}と'+f'{ans}の違い')
+    a = model.similarity(w1, ans)
+    end2 = time.time()
+    print(str(a*100/2+50)+"%")
+    print("所要時間(比較): "+ str(end2-start1))
+
+print("所要時間(モデルダウンロード): "+ str(end-start))
