f63c78f3bdcf0eed0c5f884d7afbdc2bd55dba44
CFaiter <42732153+CFaiter@users.noreply.github.com>
2023-03-24 00:59:52 +0900

    類似度のパーセンテージを四捨五入する

--- Chatle/wordTovec.py
+++ Chatle/wordTovec.py
@@ -17,8 +17,11 @@ while(True):
         break
     print(f'{w1}と'+f'{ans}の違い')
     target = model.similarity(w1, ans)
+    if(target < 0):
+        target = 0
     end2 = time.time()
-    print(str(target*100/2+50)+"%")
+    ans = str(Decimal(str(target*100)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+    print(ans+"%")
     print("所要時間(比較): "+ str(end2-start1))
 
 print("所要時間(モデルダウンロード): "+ str(end-start))
